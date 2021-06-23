7f018fb054dca52b1fb1b4dd5f542909b37347d8
josygeorge <joe.george.josy@gmail.com>
2021-06-23 06:14:52 +0000

    templates file created

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
@@ -7,11 +11,20 @@ if os.path.exists("env.py"):
 
 # create instance of Flask
 app = Flask(__name__)
+# Setting environment variables
+app.config["MONGO_DATABASE"] = os.environ.get("MONGO_DATABASE")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+# instance of mongo
+mongo = PyMongo(app)
 
 
+# Routes
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/list-words')
+def list_words():
+    words = mongo.db.words.find()
+    return render_template('glossary.html', words=words)
 
 
 if __name__ == "__main__":
